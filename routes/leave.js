979d7bb1f99b438fd7368023864c7c0b7ca50758
Ateeq ullah <ateequllah5656@gmail.com>
2024-12-02 12:09:43 +0500

    backend code

---
+++ routes/leave.js
@@ -0,0 +1,16 @@
+import express from "express"
+import authMiddleware from "../middleware/authMiddleware.js"
+import { addLeave, getLeaves, getLeave, getLeaveDetail, updateLeave } from "../controllers/leaveController.js"
+
+
+
+const router = express.Router()
+
+router.post("/add", authMiddleware, addLeave)
+router.get("/detail/:id", authMiddleware, getLeaveDetail)
+router.get("/:id/:role", authMiddleware, getLeave)
+router.get("/", authMiddleware, getLeaves)
+router.put("/:id", authMiddleware, updateLeave)
+
+
+export default router
